20b7107eae6fe2a6712f5397a2cc944259fb8697
kundaiiii <kundaimm@yahoo.com>
2023-07-07 03:45:46 +0200

    Tasks + README.md

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''Given the parameters and the return values, add type annotations to the function
+
+Hint: look into TypeVar
+
+def safely_get_value(dct, key, default = None):
+    if key in dct:
+        return dct[key]
+    else:
+        return default
+'''
+from typing import Any, Mapping, Union, TypeVar
+
+
+T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    '''Retrieves a value from a dict using a given key.
+    '''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''a type-annotated function floor which takes a float n as argument and returns the floor of the float
+'''
+
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return int(a)
